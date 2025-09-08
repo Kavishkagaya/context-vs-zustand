626057cd5df7d3a6277e69518b6b0b3c8de1df92
Kavishka <kavishkarambukwella@gmail.com>
2025-09-08 18:37:05 +0530

    fixed the rerendering by pricetag

--- src/components/product/StoreProductContainer.tsx
+++ src/components/product/StoreProductContainer.tsx
@@ -2,24 +2,25 @@ import ProductCard from "./ProductCard";
 import type { ProductType } from "../../types";
 import { useProductStore } from "../../store/ProductStore";
 import StorePriceTag from "../pricetag/StorePriceTag";
-import { memo, useCallback } from "react";
+import { memo } from "react";
 import { useProductActions } from "../../hooks/useProductActions";
+
+const MemoPriceTag = memo(StorePriceTag);
 
 const StoreProductContainer:React.FC = () => {
     const products = useProductStore((state) => state.products);
     const addToCart = useProductActions().addToCart;
 
-    const handleAddToCart = useCallback((id: number, quantity: number) => {
-        addToCart(id, quantity);
-    }, [addToCart]);
+
+    console.log("container rerenders");
 
     return (
         <div className="grid">
             {Object.entries(products).map(([id, product]: [string, ProductType]) => {
                 const currentProduct = useProductStore(state => state.products[Number(id)]);
                 return (
-                    <ProductCard key={id} product={currentProduct} addToCart={handleAddToCart} >
-                        <StorePriceTag price={currentProduct.price} />
+                    <ProductCard key={id} product={currentProduct} addToCart={addToCart} >
+                        <MemoPriceTag price={currentProduct.price} />
                     </ProductCard>
                 );
             })}
